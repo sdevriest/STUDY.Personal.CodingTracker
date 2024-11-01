b7bd705c72031a05015edbcbda799cf688133665
sdevriest <sebastian.devries.t@gmail.com>
2024-11-01 18:50:11 +0100

    Initial Commit

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -6,5 +6,20 @@
 		{
 			Console.WriteLine("Hello, C# Academy! One more modification");
 		}
+
+		void MyFirstMethod()
+		{
+			// Do Something
+		}
+
+		void MySecondMethod()
+		{
+			// Do Something
+		}
+
+		void MyThirdMethod()
+		{
+			// Do Something
+		}
 	}
 }
